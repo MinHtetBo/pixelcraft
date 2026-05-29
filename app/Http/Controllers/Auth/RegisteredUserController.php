489145bf896a1dc47e8826a00485f8ac0be1986e
Local XAMPP User <localuser@localhost.com>
2026-05-30 00:19:07 +0630

    Fix redirect route with hash

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -50,7 +50,7 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return to_route("user#home");
+        return redirect("/user#home");
 
         //return redirect(RouteServiceProvider::HOME);
 
